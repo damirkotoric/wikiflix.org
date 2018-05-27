ec17702a2ebbda5fb4836ae251664c4a308345b0
Damir Kotoric <damirkotoric@gmail.com>
2018-05-28 01:15:55 +0200

    working frontend

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -13,24 +13,39 @@ window.onload = function () {
 
   // Variable definitions
   var instamentaryData = {}
+  var inputForm = document.getElementById('input__form')
+  var subscribeForm = document.getElementById('subscribe__form')
+  var inputUrl = document.getElementById('input__url')
+  var email = document.getElementById('email')
+  var instamentaryControls = document.getElementById('instamentary__controls')
   var controls = document.getElementById('controls')
-  var playButton = document.getElementById('controls__play')
-  var pauseButton = document.getElementById('controls__pause')
+  var playButton = document.getElementById('play')
+  var pauseButton = document.getElementById('pause')
+  var player = document.getElementById('player')
   var playerAudio = document.getElementById('player__audio')
   var playerImage = document.getElementById('player__image')
+  var playerBackground = document.getElementById('player__background')
   var snippets = document.getElementById('snippets')
+  var modalClose = document.getElementById('modal__close')
+  var subscribe = document.getElementById('subscribe')
+  var subscribeOutputDone = document.getElementById('subscribe__output__done')
   var currentSnippetIndex = 0
 
   // Event listeners
+  inputForm.addEventListener('submit', inputFormSubmitted)
+  subscribeForm.addEventListener('submit', subscribeFormSubmitted)
   playButton.addEventListener('click', play)
   pauseButton.addEventListener('click', pause)
   playerAudio.addEventListener('ended', end)
   Array.prototype.forEach.call(snippets.querySelectorAll('.instamentary__snippet'), function(instamentarySnippet, i) {
     instamentarySnippet.addEventListener('click', jumpTo)
   })
+  window.addEventListener('scroll', scrolling)
+  modalClose.addEventListener('click', hideModal)
+  subscribeOutputDone.addEventListener('click', hideModal)
 
   // Init
-  loadData('/instamentaries/gesture-talks/info.json')
+  loadData('/data/gesture-talks/info.json')
 
   // Functions
   function loadData(url) {
@@ -73,9 +88,17 @@ window.onload = function () {
       console.log(snippets.querySelector('.instamentary__snippet:nth-of-type(' + parseInt(currentSnippetIndex)+1 + ')'))
       snippets.querySelector('.instamentary__snippet:nth-of-type(' + parseInt(currentSnippetIndex+1) + ')').classList.add('--active')
       // Set active image
-      console.log('image url is ' + instamentaryData.snippets[currentSnippetIndex].imageURL)
-      var imageURL = instamentaryData.snippets[currentSnippetIndex].imageURL
+      console.log('image url is ' + instamentaryData.snippets[currentSnippetIndex].image.url)
+      var imageURL = instamentaryData.snippets[currentSnippetIndex].image.url
+      playerImage.setAttribute('src', '')
       playerImage.setAttribute('src', imageURL)
+      playerBackground.setAttribute('src', '')
+      playerBackground.setAttribute('src', imageURL)
+      if (instamentaryData.snippets[currentSnippetIndex].image.style) {
+        player.classList.remove('--style-cover')
+        player.classList.remove('--style-contain')
+        player.classList.add('--style-' + instamentaryData.snippets[currentSnippetIndex].image.style)
+      }
     }
   }
 
@@ -103,6 +126,57 @@ window.onload = function () {
       }
     })
     play(null)
+  }
+
+  function scrolling(e) {
+    if (window.pageYOffset < instamentaryControls.offsetTop) {
+      controls.classList.remove('--sticky')
+      console.log('normal')
+    } else {
+      controls.classList.add('--sticky')
+      console.log('sticky')
+    }
+  }
+
+  function inputFormSubmitted(e) {
+    e.preventDefault()
+    showModal(e)
+  }
+
+  function subscribeFormSubmitted(e) {
+    e.preventDefault()
+    var request = new XMLHttpRequest()
+    var url = 'https://hooks.zapier.com/hooks/catch/437741/a8fgqq?email=' + email.value + '&url=' + inputUrl.value
+    subscribe.classList.add('--loading')
+    request.open('GET', url, true)
+    request.onload = function() {
+      if (this.status >= 200 && this.status < 400) {
+        // Success!
+        console.log(this.response)
+        subscribe.classList.remove('--loading')
+        subscribe.classList.add('--subscribed')
+      } else {
+        // We reached our target server, but it returned an error.
+        console.log('We reached our target server, but it returned an error.')
+      }
+    }
+    request.onerror = function() {
+      // There was a connection error of some sort.
+      console.log('There was a connection error of some sort.')
+    }
+    request.send()
+    console.log()
+    console.log(email.value)
+  }
+
+  function showModal(e) {
+    document.body.classList.remove('--modal-hide')
+    document.body.classList.add('--modal-show')
+  }
+
+  function hideModal(e) {
+    document.body.classList.remove('--modal-show')
+    document.body.classList.add('--modal-hide')
   }
 
   // Helpers
